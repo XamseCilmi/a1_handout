99f8816e5cd2fc874dbbb7b2b8a6248f61eca28a
Christian <46445109+CFjordstrom@users.noreply.github.com>
2023-11-28 12:13:11 +0100

    Add files via upload

--- numbers.h
+++ numbers.h
@@ -12,38 +12,109 @@ struct bits8 {
   struct bit b7;
 };
 
-struct bits8 bits8_from_int(unsigned int x){
-   struct bits8 n;
+struct bits8 bits8_from_int(unsigned int x) {
+  //assert(x < 255);
 
-    // Use bitwise operations 
-    n.b0.v = bit_from_int(x & 0x01).v;
-    n.b1.v = bit_from_int(x & 0x02).v;
-    n.b2.v = bit_from_int(x & 0x04).v;
-    n.b3.v = bit_from_int(x & 0x08).v;
-    n.b4.v = bit_from_int(x & 0x10).v;
-    n.b5.v = bit_from_int(x & 0x20).v;
-    n.b6.v = bit_from_int(x & 0x40).v;
-    n.b7.v = bit_from_int(x & 0x80).v;
+  struct bits8 v;
 
-    return n;
-};
-unsigned int bits8_to_int(struct bits8 x);
-void bits8_print(struct bits8 v);
-struct bits8 bits8_add(struct bits8 x, struct bits8 y){
-  struct bits8 result;
-  struct bit carry;
+  v.b0 = bit_from_int(x % 2);
+  x /= 2;
+  v.b1 = bit_from_int(x % 2);
+  x /= 2;
+  v.b2 = bit_from_int(x % 2);
+  x /= 2;
+  v.b3 = bit_from_int(x % 2);
+  x /= 2;
+  v.b4 = bit_from_int(x % 2);
+  x /= 2;
+  v.b5 = bit_from_int(x % 2);
+  x /= 2;
+  v.b6 = bit_from_int(x % 2);
+  x /= 2;
+  v.b7 = bit_from_int(x % 2);
 
-  result.b0.v = bit_or(x.b0, y.b0);
-  carry = bit_and(x.b0, y.b0)
-  result.b1.v = bit_or(x.b1,y.b1);
-
-};
-struct bits8 bits8_negate(struct bits8 x);
-struct bits8 bits8_mul(struct bits8 x, struct bits8 y);
-
-
-int main() {
-  // TODO
-  
-
+  return v;
 }
+
+unsigned int bits8_to_int(struct bits8 x) {
+  unsigned int result = 0;
+
+  result |= bit_to_int(x.b0);
+  result |= bit_to_int(x.b1) << 1;
+  result |= bit_to_int(x.b2) << 2;
+  result |= bit_to_int(x.b3) << 3;
+  result |= bit_to_int(x.b4) << 4;
+  result |= bit_to_int(x.b5) << 5;
+  result |= bit_to_int(x.b6) << 6;
+  result |= bit_to_int(x.b7) << 7;
+
+  return result;
+}
+
+void bits8_print(struct bits8 v) {
+  bit_print(v.b7);
+  bit_print(v.b6);
+  bit_print(v.b5);
+  bit_print(v.b4);
+  bit_print(v.b3);
+  bit_print(v.b2);
+  bit_print(v.b1);
+  bit_print(v.b0);
+}
+
+struct bits8 bits8_add(struct bits8 x, struct bits8 y) {
+  struct bits8 s;
+
+  struct bit c_minus_1;
+
+  c_minus_1.v = false;
+  s.b0 = bit_xor(bit_xor(x.b0, y.b0), c_minus_1);
+  struct bit c0 = bit_or(bit_and(x.b0, y.b0), bit_and(bit_or(x.b0, y.b0), c_minus_1));
+
+  s.b1 = bit_xor(bit_xor(x.b1, y.b1), c0);
+  struct bit c1 = bit_or(bit_and(x.b1, y.b1), bit_and(bit_or(x.b1, y.b1), c0));
+
+  s.b2 = bit_xor(bit_xor(x.b2, y.b2), c1);
+  struct bit c2 = bit_or(bit_and(x.b2, y.b2), bit_and(bit_or(x.b2, y.b2), c1));
+
+  s.b3 = bit_xor(bit_xor(x.b3, y.b3), c2);
+  struct bit c3 = bit_or(bit_and(x.b3, y.b3), bit_and(bit_or(x.b3, y.b3), c2));
+
+  s.b4 = bit_xor(bit_xor(x.b4, y.b4), c3);
+  struct bit c4 = bit_or(bit_and(x.b4, y.b4), bit_and(bit_or(x.b4, y.b4), c3));
+
+  s.b5 = bit_xor(bit_xor(x.b5, y.b5), c4);
+  struct bit c5 = bit_or(bit_and(x.b5, y.b5), bit_and(bit_or(x.b5, y.b5), c4));
+
+  s.b6 = bit_xor(bit_xor(x.b6, y.b6), c5);
+  struct bit c6 = bit_or(bit_and(x.b6, y.b6), bit_and(bit_or(x.b6, y.b6), c5));
+
+  s.b7 = bit_xor(bit_xor(x.b7, y.b7), c6);
+
+  return s;
+}
+
+struct bits8 bits8_negate(struct bits8 x) {
+  x.b0 = bit_not(x.b0);
+  x.b1 = bit_not(x.b1);
+  x.b2 = bit_not(x.b2);
+  x.b3 = bit_not(x.b3);
+  x.b4 = bit_not(x.b4);
+  x.b5 = bit_not(x.b5);
+  x.b6 = bit_not(x.b6);
+  x.b7 = bit_not(x.b7);
+
+  struct bits8 one;
+  one.b0 = bit_from_int(1);
+  one.b1 = bit_from_int(0);
+  one.b2 = bit_from_int(0);
+  one.b3 = bit_from_int(0);
+  one.b4 = bit_from_int(0);
+  one.b5 = bit_from_int(0);
+  one.b6 = bit_from_int(0);
+  one.b7 = bit_from_int(0);
+
+  return bits8_add(x, one);
+}
+
+struct bits8 bits8_mul(struct bits8 x, struct bits8 y);
--- test_numbers.c
+++ test_numbers.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include "numbers.h"
 
+void test_bits8_add(unsigned int x, unsigned int y) {
+  unsigned int got = bits8_to_int(bits8_add(bits8_from_int(x), bits8_from_int(y)));
+  unsigned int expected = x + y;
+  if (got != expected) {
+    printf("Input:     %u & %u\n", x, y);
+    printf("Got:       %u\n", got);
+    printf("Expected:  %u\n", expected);
+    exit(1);
+  }
+}
+
 int main() {
-  // TODO
+  assert(bits8_to_int(bits8_from_int(100)) == 100);
+  test_bits8_add(0, 0);
+  test_bits8_add(0, 10);
+  test_bits8_add(10, 0);
+  test_bits8_add(10, 10);
+  test_bits8_add(100, 155);
+
+  assert(bits8_to_int(bits8_negate(bits8_negate(bits8_from_int(0)))) == 0);
+  assert(bits8_to_int(bits8_negate(bits8_negate(bits8_from_int(10)))) == 10);
+  int minus_one = -1;
+  struct bits8 one = bits8_negate(bits8_from_int((unsigned int)minus_one));
+  int one_int = (int)bits8_to_int(one);
+  assert(one_int == 1);
 }
